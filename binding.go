4f53bad2a3a63a5e5425dc91cfb4a40932ca01cf
ᴜɴᴋɴᴡᴏɴ <u@gogs.io>
2021-08-22 00:22:10 +0800

    binding: add `Form` middleware (#8)

--- binding.go
+++ binding.go
@@ -6,13 +6,16 @@ package binding
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/url"
 	"reflect"
+	"strconv"
 
 	"github.com/flamego/flamego"
 	"github.com/flamego/validator"
 )
 
-// Options is a struct for specifying configuration options for the binding middleware.
+// Options contains options for binding.JSON, binding.Form middleware.
 type Options struct {
 	// ErrorHandler will be invoked when errors occurred.
 	ErrorHandler flamego.Handler
@@ -27,58 +30,6 @@ type errorHandlerInvoker func(flamego.Context, Errors)
 func (invoke errorHandlerInvoker) Invoke(args []interface{}) ([]reflect.Value, error) {
 	invoke(args[0].(flamego.Context), args[1].(Errors))
 	return nil, nil
-}
-
-// JSON returns a middleware handler that injects a new instance of the model
-// with populated fields and binding.Errors for any deserialization,
-// binding, or validation errors into the request context. The model instance
-// fields are populated by deserializing the JSON payload from the request body.
-func JSON(model interface{}, opts ...Options) flamego.Handler {
-	var option Options
-	if len(opts) == 1 {
-		option = opts[0]
-	}
-
-	if option.Validator == nil {
-		option.Validator = validator.New()
-	}
-
-	var errorHandler flamego.Handler
-	switch v := option.ErrorHandler.(type) {
-	case func(flamego.Context, Errors):
-		errorHandler = errorHandlerInvoker(v)
-	default:
-		errorHandler = v
-	}
-
-	ensureNotPointer(model)
-	validate := option.Validator
-	return flamego.ContextInvoker(func(c flamego.Context) {
-		var errs Errors
-		obj := reflect.New(reflect.TypeOf(model))
-		r := c.Request().Request
-		if r.Body != nil {
-			defer func() { _ = r.Body.Close() }()
-			err := json.NewDecoder(r.Body).Decode(obj.Interface())
-			if err != nil {
-				errs = append(errs,
-					Error{
-						Category: ErrorCategoryDeserialization,
-						Err:      err,
-					},
-				)
-			}
-		}
-		validateAndMap(c, validate, obj, errs)
-
-		errs = c.Value(reflect.TypeOf(errs)).Interface().(Errors)
-		if len(errs) > 0 && errorHandler != nil {
-			_, err := c.Invoke(errorHandler)
-			if err != nil {
-				panic("binding: " + err.Error())
-			}
-		}
-	})
 }
 
 // ensureNotPointer panics if the given value is a pointer.
@@ -101,4 +52,242 @@ func validateAndMap(c flamego.Context, validate *validator.Validate, obj reflect
 		)
 	}
 	c.Map(errs, obj.Elem().Interface())
+}
+
+func parseOptions(opts Options) Options {
+	switch v := opts.ErrorHandler.(type) {
+	case func(flamego.Context, Errors):
+		opts.ErrorHandler = errorHandlerInvoker(v)
+	}
+
+	if opts.Validator == nil {
+		opts.Validator = validator.New()
+	}
+	return opts
+}
+
+// JSON returns a middleware handler that injects a new instance of the model
+// with populated fields and binding.Errors for any deserialization, binding, or
+// validation errors into the request context. The model instance fields are
+// populated by deserializing the JSON payload from the request body.
+func JSON(model interface{}, opts ...Options) flamego.Handler {
+	ensureNotPointer(model)
+
+	var opt Options
+	if len(opts) > 0 {
+		opt = opts[0]
+	}
+	opt = parseOptions(opt)
+
+	return flamego.ContextInvoker(func(c flamego.Context) {
+		var errs Errors
+		obj := reflect.New(reflect.TypeOf(model))
+		r := c.Request().Request
+		if r.Body != nil {
+			defer func() { _ = r.Body.Close() }()
+			err := json.NewDecoder(r.Body).Decode(obj.Interface())
+			if err != nil {
+				errs = append(errs,
+					Error{
+						Category: ErrorCategoryDeserialization,
+						Err:      err,
+					},
+				)
+			}
+		}
+		validateAndMap(c, opt.Validator, obj, errs)
+
+		errs = c.Value(reflect.TypeOf(errs)).Interface().(Errors)
+		if len(errs) > 0 && opt.ErrorHandler != nil {
+			_, err := c.Invoke(opt.ErrorHandler)
+			if err != nil {
+				panic("binding.JSON: " + err.Error())
+			}
+		}
+	})
+}
+
+// Form returns a middleware handler that injects a new instance of the model
+// with populated fields and binding.Errors for any deserialization, binding, or
+// validation errors into the request context. The model instance fields are
+// populated by deserializing the payload from both form-urlencoded data request
+// body and URL query parameters.
+func Form(model interface{}, opts ...Options) flamego.Handler {
+	ensureNotPointer(model)
+
+	var opt Options
+	if len(opts) > 0 {
+		opt = opts[0]
+	}
+	opt = parseOptions(opt)
+
+	return flamego.ContextInvoker(func(c flamego.Context) {
+		var errs Errors
+		r := c.Request().Request
+		err := r.ParseForm()
+		if err != nil {
+			errs = append(errs,
+				Error{
+					Category: ErrorCategoryDeserialization,
+					Err:      err,
+				},
+			)
+		}
+
+		obj := reflect.New(reflect.TypeOf(model))
+		errs = mapForm(obj, r.Form, errs)
+		validateAndMap(c, opt.Validator, obj, errs)
+
+		errs = c.Value(reflect.TypeOf(errs)).Interface().(Errors)
+		if len(errs) > 0 && opt.ErrorHandler != nil {
+			_, err := c.Invoke(opt.ErrorHandler)
+			if err != nil {
+				panic("binding.Form: " + err.Error())
+			}
+		}
+	})
+}
+
+// mapForm takes values from the form data and maps them into the struct object.
+func mapForm(
+	obj reflect.Value,
+	form url.Values,
+	errs Errors,
+) Errors {
+	if obj.Kind() == reflect.Ptr {
+		obj = obj.Elem()
+	}
+	typ := obj.Type()
+
+	for i := 0; i < typ.NumField(); i++ {
+		typeField := typ.Field(i)
+		structField := obj.Field(i)
+		if !structField.CanSet() {
+			continue
+		}
+
+		if typeField.Type.Kind() == reflect.Ptr && typeField.Anonymous {
+			structField.Set(reflect.New(typeField.Type.Elem()))
+			errs = mapForm(structField.Elem(), form, errs)
+			if reflect.DeepEqual(structField.Elem().Interface(), reflect.Zero(structField.Elem().Type()).Interface()) {
+				structField.Set(reflect.Zero(structField.Type()))
+			}
+		} else if typeField.Type.Kind() == reflect.Struct {
+			errs = mapForm(structField, form, errs)
+		}
+
+		fieldName := typeField.Tag.Get("form")
+		if fieldName == "" {
+			fieldName = typeField.Name
+		}
+
+		inputValue, exists := form[fieldName]
+		if exists {
+			numElems := len(inputValue)
+			if structField.Kind() == reflect.Slice && numElems > 0 {
+				sliceOf := structField.Type().Elem().Kind()
+				slice := reflect.MakeSlice(structField.Type(), numElems, numElems)
+				for i := 0; i < numElems; i++ {
+					err := setWithProperType(sliceOf, inputValue[i], slice.Index(i), fieldName)
+					if err != nil {
+						errs = append(errs, *err)
+					}
+				}
+				obj.Field(i).Set(slice)
+			} else {
+				err := setWithProperType(typeField.Type.Kind(), inputValue[0], structField, fieldName)
+				if err != nil {
+					errs = append(errs, *err)
+				}
+			}
+			continue
+		}
+	}
+	return errs
+}
+
+// setWithProperType sets the value of an indeterminate type to the matching
+// value from the request in the same type, so that not all deserialized values
+// have to be strings. Supported types are int, uint, bool, float and string.
+func setWithProperType(kind reflect.Kind, val string, field reflect.Value, name string) *Error {
+	switch kind {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		if val == "" {
+			val = "0"
+		}
+		parsed, err := strconv.ParseInt(val, 10, 64)
+		if err != nil {
+			return &Error{
+				Category: ErrorCategoryDeserialization,
+				Err:      fmt.Errorf("field %q cannot parse %q as int", name, val),
+			}
+		}
+
+		field.SetInt(parsed)
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		if val == "" {
+			val = "0"
+		}
+		parsed, err := strconv.ParseUint(val, 10, 64)
+		if err != nil {
+			return &Error{
+				Category: ErrorCategoryDeserialization,
+				Err:      fmt.Errorf("field %q cannot parse %q as uint", name, val),
+			}
+		}
+
+		field.SetUint(parsed)
+
+	case reflect.Bool:
+		if val == "on" {
+			field.SetBool(true)
+			break
+		}
+
+		if val == "" {
+			val = "false"
+		}
+		parsed, err := strconv.ParseBool(val)
+		if err != nil {
+			return &Error{
+				Category: ErrorCategoryDeserialization,
+				Err:      fmt.Errorf("field %q cannot parse %q as bool", name, val),
+			}
+		}
+
+		field.SetBool(parsed)
+
+	case reflect.Float32:
+		if val == "" {
+			val = "0.0"
+		}
+		parsed, err := strconv.ParseFloat(val, 32)
+		if err != nil {
+			return &Error{
+				Category: ErrorCategoryDeserialization,
+				Err:      fmt.Errorf("field %q cannot parse %q as float32", name, val),
+			}
+		}
+
+		field.SetFloat(parsed)
+
+	case reflect.Float64:
+		if val == "" {
+			val = "0.0"
+		}
+		parsed, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			return &Error{
+				Category: ErrorCategoryDeserialization,
+				Err:      fmt.Errorf("field %q cannot parse %q as float64", name, val),
+			}
+		}
+
+		field.SetFloat(parsed)
+
+	case reflect.String:
+		field.SetString(val)
+	}
+	return nil
 }
--- binding_test.go
+++ binding_test.go
@@ -155,7 +155,7 @@ func TestJSON(t *testing.T) {
 
 	tests := []struct {
 		name         string
-		body         interface{}
+		body         user
 		assertErrors func(t *testing.T, errs Errors)
 	}{
 		{
@@ -247,6 +247,359 @@ func TestJSON(t *testing.T) {
 
 			test.assertErrors(t, gotErrs)
 			assert.Equal(t, test.body, gotForm)
+		})
+	}
+}
+
+func TestForm(t *testing.T) {
+	t.Run("pointer model", func(t *testing.T) {
+		assert.PanicsWithValue(t,
+			"binding: pointer can not be accepted as binding model",
+			func() {
+				type form struct {
+					Username string
+					Password string
+				}
+				Form(&form{})
+			},
+		)
+	})
+
+	t.Run("custom error handler", func(t *testing.T) {
+		type form struct {
+			Username string `validate:"required"`
+			Password string `validate:"required"`
+		}
+
+		normalHandler := func(rw http.ResponseWriter, errs Errors) {
+			rw.WriteHeader(http.StatusBadRequest)
+			_, _ = rw.Write([]byte(errs[0].Err.Error()))
+		}
+
+		fastInvokerHandler := func(c flamego.Context, errs Errors) {
+			c.ResponseWriter().WriteHeader(http.StatusBadRequest)
+			_, _ = c.ResponseWriter().Write([]byte(fmt.Sprintf("Oops! Error occurred: %v", errs[0].Err)))
+		}
+
+		tests := []struct {
+			name       string
+			payload    []byte
+			handler    flamego.Handler
+			statusCode int
+			want       string
+		}{
+			{
+				name:       "validation error",
+				payload:    []byte(`Username=alice`),
+				handler:    fastInvokerHandler,
+				statusCode: http.StatusBadRequest,
+				want:       "Oops! Error occurred: Key: 'form.Password' Error:Field validation for 'Password' failed on the 'required' tag",
+			},
+			{
+				name:       "normal handler",
+				payload:    []byte(`Username=alice`),
+				handler:    normalHandler,
+				statusCode: http.StatusBadRequest,
+				want:       "Key: 'form.Password' Error:Field validation for 'Password' failed on the 'required' tag",
+			},
+			{
+				name:       "fast invoker handler",
+				payload:    []byte(`Username=alice&Password=supersecurepassword`),
+				handler:    fastInvokerHandler,
+				statusCode: http.StatusOK,
+				want:       "Hello world",
+			},
+			{
+				name:       "nil handler",
+				payload:    []byte(`Username=alice`),
+				handler:    nil,
+				statusCode: http.StatusOK,
+				want:       "Hello world",
+			},
+		}
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				f := flamego.New()
+				opts := Options{
+					ErrorHandler: test.handler,
+				}
+				f.Post("/", Form(form{}, opts), func(c flamego.Context) {
+					_, _ = c.ResponseWriter().Write([]byte("Hello world"))
+				})
+
+				resp := httptest.NewRecorder()
+				req, err := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer(test.payload))
+				assert.Nil(t, err)
+
+				req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+				f.ServeHTTP(resp, req)
+				assert.Equal(t, test.statusCode, resp.Code)
+				assert.Equal(t, test.want, resp.Body.String())
+			})
+		}
+	})
+
+	type address struct {
+		Street string `form:"street" validate:"required"`
+		City   string `form:"city" validate:"required"`
+		Planet string `form:"planet" validate:"required"`
+		Phone  string `form:"phone" validate:"required"`
+	}
+	type user struct {
+		FirstName string   `form:"first_name" validate:"required"`
+		LastName  string   `form:"last_name" validate:"required"`
+		Age       uint8    `form:"age" validate:"gte=0,lte=130"`
+		Height    int      `form:"height" validate:"gte=0"`
+		Male      bool     `form:"male"`
+		Email     string   `form:"email" validate:"required,email"`
+		Weight    float32  `form:"weight" validate:"gte=0"`
+		Balance   float64  `form:"balance"`
+		Address   address  `form:"address" validate:"required,dive,required"`
+		IPs       []string `form:"ip" validate:"dive,ip"`
+	}
+
+	tests := []struct {
+		name         string
+		body         string
+		want         user
+		assertErrors func(t *testing.T, errs Errors)
+	}{
+		{
+			name: "good",
+			body: `first_name=Logan&last_name=Smith&age=17&height=170&male=true&email=logan.smith@example.com&weight=60.7&balance=-12.4` +
+				`&street=404 Broadway&city=Browser&planet=Internet&phone=886` +
+				`&ip=192.168.1.1`,
+			want: user{
+				FirstName: "Logan",
+				LastName:  "Smith",
+				Age:       17,
+				Height:    170,
+				Male:      true,
+				Email:     "logan.smith@example.com",
+				Weight:    60.7,
+				Balance:   -12.4,
+				Address: address{
+					Street: "404 Broadway",
+					City:   "Browser",
+					Planet: "Internet",
+					Phone:  "886",
+				},
+				IPs: []string{"192.168.1.1"},
+			},
+			assertErrors: func(t *testing.T, errs Errors) {
+				assert.Len(t, errs, 0)
+			},
+		},
+		{
+			name: "bad int",
+			body: `first_name=Logan&last_name=Smith&age=17&height=bad&male=true&email=logan.smith@example.com&weight=60.7&balance=-12.4` +
+				`&street=404 Broadway&city=Browser&planet=Internet&phone=886`,
+			want: user{
+				FirstName: "Logan",
+				LastName:  "Smith",
+				Age:       17,
+				Height:    0,
+				Male:      true,
+				Email:     "logan.smith@example.com",
+				Weight:    60.7,
+				Balance:   -12.4,
+				Address: address{
+					Street: "404 Broadway",
+					City:   "Browser",
+					Planet: "Internet",
+					Phone:  "886",
+				},
+			},
+			assertErrors: func(t *testing.T, errs Errors) {
+				assert.Len(t, errs, 1)
+
+				got := fmt.Sprintf("%v", errs[0])
+				want := `{deserialization field "height" cannot parse "bad" as int}`
+				assert.Equal(t, want, got)
+			},
+		},
+		{
+			name: "bad uint",
+			body: `first_name=Logan&last_name=Smith&age=bad&height=170&male=true&email=logan.smith@example.com&weight=60.7&balance=-12.4` +
+				`&street=404 Broadway&city=Browser&planet=Internet&phone=886`,
+			want: user{
+				FirstName: "Logan",
+				LastName:  "Smith",
+				Age:       0,
+				Height:    170,
+				Male:      true,
+				Email:     "logan.smith@example.com",
+				Weight:    60.7,
+				Balance:   -12.4,
+				Address: address{
+					Street: "404 Broadway",
+					City:   "Browser",
+					Planet: "Internet",
+					Phone:  "886",
+				},
+			},
+			assertErrors: func(t *testing.T, errs Errors) {
+				assert.Len(t, errs, 1)
+
+				got := fmt.Sprintf("%v", errs[0])
+				want := `{deserialization field "age" cannot parse "bad" as uint}`
+				assert.Equal(t, want, got)
+			},
+		},
+		{
+			name: "bad bool",
+			body: `first_name=Logan&last_name=Smith&age=17&height=170&male=bad&email=logan.smith@example.com&weight=60.7&balance=-12.4` +
+				`&street=404 Broadway&city=Browser&planet=Internet&phone=886`,
+			want: user{
+				FirstName: "Logan",
+				LastName:  "Smith",
+				Age:       17,
+				Height:    170,
+				Male:      false,
+				Email:     "logan.smith@example.com",
+				Weight:    60.7,
+				Balance:   -12.4,
+				Address: address{
+					Street: "404 Broadway",
+					City:   "Browser",
+					Planet: "Internet",
+					Phone:  "886",
+				},
+			},
+			assertErrors: func(t *testing.T, errs Errors) {
+				assert.Len(t, errs, 1)
+
+				got := fmt.Sprintf("%v", errs[0])
+				want := `{deserialization field "male" cannot parse "bad" as bool}`
+				assert.Equal(t, want, got)
+			},
+		},
+		{
+			name: "bad float32",
+			body: `first_name=Logan&last_name=Smith&age=17&height=170&male=true&email=logan.smith@example.com&weight=bad&balance=-12.4` +
+				`&street=404 Broadway&city=Browser&planet=Internet&phone=886`,
+			want: user{
+				FirstName: "Logan",
+				LastName:  "Smith",
+				Age:       17,
+				Height:    170,
+				Male:      true,
+				Email:     "logan.smith@example.com",
+				Weight:    0,
+				Balance:   -12.4,
+				Address: address{
+					Street: "404 Broadway",
+					City:   "Browser",
+					Planet: "Internet",
+					Phone:  "886",
+				},
+			},
+			assertErrors: func(t *testing.T, errs Errors) {
+				assert.Len(t, errs, 1)
+
+				got := fmt.Sprintf("%v", errs[0])
+				want := `{deserialization field "weight" cannot parse "bad" as float32}`
+				assert.Equal(t, want, got)
+			},
+		},
+		{
+			name: "bad float64",
+			body: `first_name=Logan&last_name=Smith&age=17&height=170&male=true&email=logan.smith@example.com&weight=60.7&balance=bad` +
+				`&street=404 Broadway&city=Browser&planet=Internet&phone=886`,
+			want: user{
+				FirstName: "Logan",
+				LastName:  "Smith",
+				Age:       17,
+				Height:    170,
+				Male:      true,
+				Email:     "logan.smith@example.com",
+				Weight:    60.7,
+				Balance:   0,
+				Address: address{
+					Street: "404 Broadway",
+					City:   "Browser",
+					Planet: "Internet",
+					Phone:  "886",
+				},
+			},
+			assertErrors: func(t *testing.T, errs Errors) {
+				assert.Len(t, errs, 1)
+
+				got := fmt.Sprintf("%v", errs[0])
+				want := `{deserialization field "balance" cannot parse "bad" as float64}`
+				assert.Equal(t, want, got)
+			},
+		},
+		{
+			name: "default values",
+			body: `first_name=Logan&last_name=Smith&age=&height=&male=&email=logan.smith@example.com&weight=&balance=` +
+				`&street=404 Broadway&city=Browser&planet=Internet&phone=886`,
+			want: user{
+				FirstName: "Logan",
+				LastName:  "Smith",
+				Age:       0,
+				Height:    0,
+				Male:      false,
+				Email:     "logan.smith@example.com",
+				Weight:    0,
+				Balance:   0,
+				Address: address{
+					Street: "404 Broadway",
+					City:   "Browser",
+					Planet: "Internet",
+					Phone:  "886",
+				},
+			},
+			assertErrors: func(t *testing.T, errs Errors) {
+				assert.Len(t, errs, 0)
+			},
+		},
+		{
+			name: "bool on",
+			body: `first_name=Logan&last_name=Smith&age=17&height=170&male=on&email=logan.smith@example.com&weight=60.7&balance=-12.4` +
+				`&street=404 Broadway&city=Browser&planet=Internet&phone=886` +
+				`&ip=192.168.1.1`,
+			want: user{
+				FirstName: "Logan",
+				LastName:  "Smith",
+				Age:       17,
+				Height:    170,
+				Male:      true,
+				Email:     "logan.smith@example.com",
+				Weight:    60.7,
+				Balance:   -12.4,
+				Address: address{
+					Street: "404 Broadway",
+					City:   "Browser",
+					Planet: "Internet",
+					Phone:  "886",
+				},
+				IPs: []string{"192.168.1.1"},
+			},
+			assertErrors: func(t *testing.T, errs Errors) {
+				assert.Len(t, errs, 0)
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var gotForm user
+			var gotErrs Errors
+			f := flamego.New()
+			f.Post("/", Form(user{}), func(form user, errs Errors) {
+				gotForm = form
+				gotErrs = errs
+			})
+
+			resp := httptest.NewRecorder()
+			req, err := http.NewRequest(http.MethodPost, "/", bytes.NewBufferString(test.body))
+			assert.Nil(t, err)
+
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			f.ServeHTTP(resp, req)
+
+			test.assertErrors(t, gotErrs)
+			assert.Equal(t, test.want, gotForm)
 		})
 	}
 }
